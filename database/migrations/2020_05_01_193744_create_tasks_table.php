6e496415f25f492511a0a82840cb457b48153322
AJAH OGONNAYA BENJAMIN <35393285+Otech2018@users.noreply.github.com>
2020-05-09 12:18:42 +0100

    Add files via upload

---
+++ database/migrations/2020_05_01_193744_create_tasks_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+
+            $table->integer('user_id')->unsigned();
+            $table->integer('project_id')->unsigned();
+            $table->integer('company_id')->unsigned()->nullable();
+
+            $table->integer('days')->unsigned()->nullable();
+            $table->integer('hours')->unsigned()->nullable();
+
+         //   $table->foreign('user_id')->refernces('id')->on('users');
+         //   $table->foreign('project_id')->refernces('id')->on('projects');
+         //   $table->foreign('company_id')->refernces('id')->on('companies');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
